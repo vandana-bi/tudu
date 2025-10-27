7abf8bcaebb7c564d0d562585f7d92ea75077046
vandana-bi <v.ramchandani@braininventory.com>
2025-10-27 17:51:49 +0530

    Update auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,15 +1,6 @@
 import jwt from "jsonwebtoken";
 
-// export const authenticateJWT = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) return res.status(401).json({ message: "No token" });
-//   const token = authHeader.split(" ")[1];
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if (err) return res.status(403).json({ message: "Invalid token" });
-//     req.user = user;
-//     next();
-//   });
-// };
+
 
 export const authenticateJWT = (req, res, next) => {
   let token;
